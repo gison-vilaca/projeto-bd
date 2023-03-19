c41bc514988e5cf5c27505fd3f34560a80b7e6ec
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2023-03-19 17:58:17 -0300

    criando os models

--- src/main/java/Models/Main.java
+++ src/main/java/Models/Main.java
@@ -6,5 +6,7 @@ public class Main {
         Cliente c1 = new Cliente("nome1", "pais","estado"," cidade", 500,
         "vliente@1","874903076");
         System.out.println(c1.toString());
+
+
     }
 }
---
+++ src/main/java/Models/Pedido_Produto.java
@@ -0,0 +1,53 @@
+package Models;
+
+public class Pedido_Produto {
+
+    private int id_pedido;
+    private int quantidade;
+    private int id_produto;
+    private int preco_produto;
+
+    public Pedido_Produto(int quantidade, int id_produto, int preco_produto) {
+        this.quantidade = quantidade;
+        this.id_produto = id_produto;
+        this.preco_produto = preco_produto;
+    }
+
+    public int getId_pedido() {
+        return id_pedido;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public int getPreco_produto() {
+        return preco_produto;
+    }
+
+    public void setPreco_produto(int preco_produto) {
+        this.preco_produto = preco_produto;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido_Produto{" +
+                "id_pedido=" + id_pedido +
+                ", quantidade=" + quantidade +
+                ", id_produto=" + id_produto +
+                ", preco_produto=" + preco_produto +
+                '}';
+    }
+}
