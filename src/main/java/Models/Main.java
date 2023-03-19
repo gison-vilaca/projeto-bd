0ab728a8d0ce9752a26eb6cb74ac3dcb976fdf76
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2023-03-19 17:08:48 -0300

    adicionando os atributos de email e telefone no model Cliente

---
+++ src/main/java/Models/Main.java
@@ -0,0 +1,10 @@
+package Models;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cliente c1 = new Cliente("nome1", "pais","estado"," cidade", 500,
+        "vliente@1","874903076");
+        System.out.println(c1.toString());
+    }
+}
