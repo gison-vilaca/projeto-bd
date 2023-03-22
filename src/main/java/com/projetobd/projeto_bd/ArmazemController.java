9950f8ce7bf03be47cfb371c06c4fc3fc7dd4549
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2023-03-22 16:39:52 -0300

    criando conecao da view com controller de Armazem

--- src/main/java/com/projetobd/projeto_bd/ArmazemController.java
+++ src/main/java/com/projetobd/projeto_bd/ArmazemController.java
@@ -1,15 +1,27 @@
 package com.projetobd.projeto_bd;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ArmazemController {
 
     @FXML
-    private Label welcomeText;
+    private Button salvo;
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private TextField txtEndereco;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application! ");
+    protected void onSalvarButtonClick(){
+
+        String nome = txtNome.getText();
+        String endereco = txtEndereco.getText();
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Armazem salvo com sucesso!");
+        alert.show();
     }
 }
