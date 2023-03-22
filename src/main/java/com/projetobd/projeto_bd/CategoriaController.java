887a94e22747f37e24706ac27eefdfd7bbd9f140
Geisianny Bernardo <74117146+Geisianny@users.noreply.github.com>
2023-03-22 16:46:33 -0300

    Criando conexao da view com controller de Categoria

--- src/main/java/com/projetobd/projeto_bd/CategoriaController.java
+++ src/main/java/com/projetobd/projeto_bd/CategoriaController.java
@@ -1,15 +1,27 @@
 package com.projetobd.projeto_bd;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class CategoriaController {
 
     @FXML
-    private Label welcomeText;
+    private Button salvo;
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private TextField txtDescricao;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application! ");
+    protected void onSalvarButtonClick() {
+
+        String nome = txtNome.getText();
+        String endereco = txtDescricao.getText();
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Categoria salva com sucesso!");
+        alert.show();
     }
 }
